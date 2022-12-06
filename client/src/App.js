f460d22fe2c8f6fefefd32d5ae496cbf00df1db3
Rahul Pawar <rahulpawar166@gmail.com>
2022-12-06 17:29:35 -0500

    User authentication added, with firebase connectivity

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import Navigation from "./components/Navigation";
+import UserAuth from "./components/UserAuth";
+
+// import PrivateRoute from "./components/PrivateRoute";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Navigation />
+        </header>
+      </div>
+      <Routes>
+        {/*<Route path="/" element={<Landing />} />
+           <Route path="/home" element={<PrivateRoute />}>
+            <Route path="/home" element={<Home />} />
+          </Route>
+          <Route path="/account" element={<PrivateRoute />}>
+            <Route path="/account" element={<Account />} />
+          </Route> */}
+        <Route path="/userauth" element={<UserAuth />} />
+      </Routes>
+    </Router>
   );
 }
 
