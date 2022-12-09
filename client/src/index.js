e973cdbd43f18957bad06f6b11347d9d6435df6c
keyursenjaliya <senjaliyakeyur@gmail.com>
2022-12-09 10:15:02 -0500

    spotify authorization and new-release and albums

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+
+require("dotenv").config();
+// console.log(process.env);
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
