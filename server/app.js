3744e92753ebb6e45af513e1452159abc6186411
keyursenjaliya <senjaliyakeyur@gmail.com>
2022-12-10 16:55:42 -0500

    change of frontend

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const configRoutes = require("./routes");
-
+const cors = require("cors");
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 // app.use("/public", express.static(__dirname + "/public"));
