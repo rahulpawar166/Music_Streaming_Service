f33479b8210a5181b512335deee712bc51e7c5da
nprkh13011 <55457971+nprkh13011@users.noreply.github.com>
2022-12-20 23:20:12 -0500

    seed file

--- server/tasks/seed.js
+++ server/tasks/seed.js
@@ -1,28 +1,40 @@
 const dbConnection = require("../config/mongoConnection");
 const data = require("../data");
-const users = data.users;
-const posts = data.posts;
+// const users = data.users;
+const playlists = data.playlists;
+
 
 (async () => {
   const db = await dbConnection.dbConnection();
   await db.dropDatabase();
 
-  const patrick = await users.addUser("Patrick", "Hill");
-  const id = patrick._id.toString();
-  await posts.addPost("Hello, class!", "Today we are creating a blog!", [], id);
-  await posts.addPost(
-    "Using the seed",
-    "We use the seed to have some initial data so we can just focus on servers this week",
-    [],
-    id,
-  );
+  console.log ("---------------------PLAYLISTS-----------------")
+  console.log("CREATE - PLAYLISTS");
+  const createproperty1 = await playlists.createPlaylist(ownerId.toString(), "testing", true, ["1DzJbVkBELu6jDu6q0T0tk"]);
+  console.log(createproperty1);
 
-  await posts.addPost(
-    "Using routes",
-    "The purpose of today is to simply look at some GET routes",
-    [],
-    id,
-  );
+  console.log("CREATE - PLAYLISTS");
+  const createproperty2 = await playlists.createPlaylist(ownerId.toString(), "testing2", true, ["1DzJbVkBELu6jDu6q0T0tk"]);
+  console.log(createproperty2);
+
+  console.log("CREATE - PLAYLISTS");
+  const createproperty3 = await playlists.createPlaylist(ownerId.toString(), "testing3", false, ["1DzJbVkBELu6jDu6q0T0tk"]);
+  console.log(createproperty3);
+
+  console.log("GET - PLAYLISTS");
+  const property4 = await playlists.getPlaylist(ownerId.toString(), createproperty1._id.toString());
+  console.log(property4);
+  console.log("GET - PLAYLISTS");
+  const property5 = await playlists.getPlaylist(ownerId.toString(), createproperty2._id.toString());
+  console.log(property5);
+
+  console.log("Delete - PLAYLISTS");
+  const property6 = await playlists.deletePlaylist(ownerId.toString(), createproperty1._id.toString());
+  console.log(property6);
+
+  console.log("Add Track - PLAYLISTS");
+  const property7 = await playlists.addTrack(ownerId.toString(), createproperty1._id.toString());
+  console.log(property7);
 
   console.log("Done seeding database");
 
