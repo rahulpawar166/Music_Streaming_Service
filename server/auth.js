2288b42f55080b53e90ba6a3034d1b0f46fb57c4
Rahul Pawar <rahulpawar166@gmail.com>
2022-12-20 22:35:39 -0500

    bug fix

--- server/auth.js
+++ server/auth.js
@@ -4,8 +4,8 @@ const { getAuth } = require("firebase-admin/auth");
 const serviceAccount = {
   type: process.env.EXPRESS_APP_TYPE,
   project_id: process.env.EXPRESS_APP_PROJECT_ID,
-  private_key_id: process.env.EXPRESS_APP_PRIVATE_KEY_ID.replace(/\n/g, '\n'),
-  private_key: process.env.EXPRESS_APP_PRIVATE_KEY,
+  private_key_id: process.env.EXPRESS_APP_PRIVATE_KEY_ID,
+  private_key: process.env.EXPRESS_APP_PRIVATE_KEY.replace(/\n/g, '\n'),
   client_email: process.env.EXPRESS_APP_CLIENT_EMAIL,
   client_id: process.env.EXPRESS_APP_CLIENT_ID,
   auth_uri: process.env.EXPRESS_APP_AUTH_URI,
